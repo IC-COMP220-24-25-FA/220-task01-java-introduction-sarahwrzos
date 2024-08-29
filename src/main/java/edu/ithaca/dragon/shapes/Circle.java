3ebf4921994f510e0f36a2312fc82de174110cfb
Sarah Wrzos <swrzos@ithaca.edu>
2024-08-29 12:43:11 -0400

    longest line within implemented

--- src/main/java/edu/ithaca/dragon/shapes/Circle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Circle.java
@@ -28,6 +28,6 @@ public class Circle {
      * @return the longest straight line that can be drawn within this circle
      */
     public double longestLineWithin(){
-        throw new RuntimeException("Not implemented yet");
+        return radius;
     }
 }
