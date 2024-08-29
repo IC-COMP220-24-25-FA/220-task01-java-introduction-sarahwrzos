1ccf9a16270d62670d30616bc0811d6a3a610aac
Sarah Wrzos <swrzos@ithaca.edu>
2024-08-29 12:55:21 -0400

    double size rectangle test done

--- src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
+++ src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
@@ -14,5 +14,12 @@ public class RectangleTest {
         double area = myRectangle.calcArea();
         assertEquals(15, area, .000001);
     }
+    @Test
+    public void doubleSizeRTest(){
+        Rectangle myRectangle = new Rectangle(2,4);
+        myRectangle.doubleSize();
+        assertEquals(4, myRectangle.length, .00001);
+        assertEquals(8, myRectangle.width, .00001);
+    }
 
 }
